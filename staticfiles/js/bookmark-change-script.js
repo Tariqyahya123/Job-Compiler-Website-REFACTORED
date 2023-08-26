a292e17c4d0637d25152af3e0f1e614c647c0ed8
tariq.y <tariq.y@sybertechnology.com>
2023-08-26 15:41:09 +0300

    fixed static config

---
+++ staticfiles/js/bookmark-change-script.js
@@ -0,0 +1,17 @@
+
+
+// for (let i = 0; i < cbox.length; i++) {
+//     cbox[i].addEventListener("click", function () {
+//         cbox[i].classList.toggle("red");
+//     });
+// }
+
+// function changeImage() {
+//     console.log('money')
+//     if (document.getElementByClassName("bookmark").src == "images/Five-pointed_star.svg") {
+//         document.getElementByClassName("bookmark").src = "images.FA_star.svg";
+//     }
+//     else {
+//         document.getElementByClassName("bookmark").src = "images/Five-pointed_star.svg";
+//     }
+// }
