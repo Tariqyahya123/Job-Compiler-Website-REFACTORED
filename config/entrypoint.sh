a5da359e9aea46cbbff19c2854b364c750be3d66
tariq.y <tariq.y@sybertechnology.com>
2023-08-27 21:20:48 +0300

    make gunicorn run in forefront not daemon

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 RUN_PORT="80"
 
-gunicorn jobs.wsgi:application --bind "0.0.0.0:${RUN_PORT}" --access-logfile - --error-logfile - --daemon
+gunicorn jobs.wsgi:application --bind "0.0.0.0:${RUN_PORT}" --access-logfile - --error-logfile -
 
 #nginx -c /etc/nginx/conf.d/default.conf -g 'daemon off;'
