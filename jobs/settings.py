3a11dca14b24240dc3816f178f92466c03a4a85b
Tariq yahya <tariq.yahya300@gmail.com>
2023-08-28 04:11:41 +0300

    Update settings.py

--- jobs/settings.py
+++ jobs/settings.py
@@ -132,11 +132,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'scrape/static') 
-)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
